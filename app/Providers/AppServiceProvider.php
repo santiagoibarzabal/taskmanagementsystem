24b6f1a5be4f7a90b00065ce6fe754a1c9545e04
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2024-02-12 20:42:32 +0100

    refactor: rename task repository interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use App\TaskManagementSystem\Status\Domain\Interfaces\StatusRepository;
 use App\TaskManagementSystem\Status\Infrastructure\MySqlStatusRepository;
-use App\TaskManagementSystem\Task\Domain\Interfaces\TaskRepositoryInterface;
+use App\TaskManagementSystem\Task\Domain\Interfaces\TaskRepository;
 use App\TaskManagementSystem\Task\Infrastructure\Repositories\MySqlTaskRepository;
 use App\TaskManagementSystem\User\Domain\Interfaces\UserRepository;
 use App\TaskManagementSystem\User\Infrastructure\Repositories\MySqlUserRepository;
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(StatusRepository::class, MySqlStatusRepository::class);
-        $this->app->bind(TaskRepositoryInterface::class, MySqlTaskRepository::class);
+        $this->app->bind(TaskRepository::class, MySqlTaskRepository::class);
         $this->app->bind(UserRepository::class, MySqlUserRepository::class);
     }
 }
