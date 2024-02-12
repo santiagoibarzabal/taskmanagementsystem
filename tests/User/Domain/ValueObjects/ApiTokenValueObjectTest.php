b9d659bcc65d698d60f384f7dee3a0e5a8f0c28d
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2024-02-12 20:36:21 +0100

    feat: add user domain tests

--- tests/User/Domain/ValueObjects/ApiTokenValueObjectTest.php
+++ tests/User/Domain/ValueObjects/ApiTokenValueObjectTest.php
@@ -2,35 +2,22 @@
 
 declare(strict_types=1);
 
-namespace Tests\User\Domain;
+namespace Tests\User\Domain\ValueObjects;
 
-use App\TaskManagementSystem\Status\Domain\StatusAggregate;
-use App\TaskManagementSystem\Status\Domain\ValueObjects\Description as StatusDescription;
-use App\TaskManagementSystem\Task\Domain\Exceptions\InvalidTaskDescriptionException;
-use App\TaskManagementSystem\Task\Domain\Exceptions\InvalidTaskTitleException;
-use App\TaskManagementSystem\Task\Domain\TaskAggregate;
-use App\TaskManagementSystem\Task\Domain\ValueObjects\Description;
-use App\TaskManagementSystem\Task\Domain\ValueObjects\Priority;
-use App\TaskManagementSystem\Task\Domain\ValueObjects\Title;
-use App\TaskManagementSystem\User\Domain\UserAggregate;
-use Illuminate\Support\Str;
+use App\TaskManagementSystem\User\Domain\ValueObjects\ApiToken;
 use Tests\UnitTestCase;
 
-class UserAggregateTest extends UnitTestCase
+class ApiTokenValueObjectTest extends UnitTestCase
 {
     public function setUp(): void
     {
         parent::setUp();
     }
 
-    public function test_create_task_aggregate_throw_invalid_description_exception(): void
+    public function test_hash(): void
     {
-        $this->expectException(InvalidTaskDescriptionException::class);
-        TaskAggregate::create(
-            Title::create('title'),
-            Description::create(Str::random(290)),
-            Priority::LOW,
-            StatusAggregate::create(StatusDescription::PENDING),
-        );
+        $apiToken = ApiToken::create();
+        $hash = ApiToken::hash($apiToken->token());
+        $this->assertTrue(ApiToken::check($apiToken->token(), $hash));
     }
 }
