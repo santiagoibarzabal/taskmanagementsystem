167bdf454d22a3fbf1043300edb4730a6f10a41f
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2024-02-12 20:44:47 +0100

    refactor: remove token_has_been_shown field

--- database/migrations/2024_02_10_152704_create_users_table.php
+++ database/migrations/2024_02_10_152704_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('password');
             $table->string('api_token')->unique()->nullable();
             $table->timestamp('token_expiration')->nullable();
-            $table->boolean('token_has_been_shown')->default(0);
             $table->timestamps();
         });
     }
